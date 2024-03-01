97439a110e04ad258b4602610b86a27df6fd549a
HaNgocDao <161784819+HaNgocDao@users.noreply.github.com>
2024-03-01 09:00:14 +0700

    Update THB1_BUOI2_ETP111.py

--- THB1_BUOI2_ETP111.py
+++ THB1_BUOI2_ETP111.py
@@ -1,22 +1,10 @@
+import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import datetime as dt
-st.title('SF Trees')
-st.write(
-"""This app analyzes trees in San Francisco
-a dataset kindly provided by SF DPW"""
-)
-trees_df = pd.read_csv('trees.csv')
-trees_df['age'] = (pd.to_datetime('today') -
-pd.to_datetime(trees_df['dat
-st.subheader('Seaborn Chart')
-fig_sb, ax_sb = plt.subplots()
-ax_sb = sns.histplot(trees_df['age'])
-plt.xlabel('Age (Days)')
-st.pyplot(fig_sb)
-st.subheader('Matploblib Chart')
-fig_mpl, ax_mpl = plt.subplots()
-ax_mpl = plt.hist(trees_df['age'])
-plt.xlabel('Age (Days)')
-st.pyplot(fig_mpl)
+import numpy as np
+st.title('Area')
+# Defining dataframe with its values
+df = pd.DataFrame(
+np.random.randn(40, 4),
+columns=["C1", "C2", "C3", "C4"])
+# Bar Chart
+st.bar_chart(df)
